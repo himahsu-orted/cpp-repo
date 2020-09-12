3517cf326503e0c26e100e61066eca1012abcf54
Himanshu Sah <hs@Himanshus-MacBook-Pro.local>
2020-09-12 20:57:52 +0545

    c++

--- a.cpp
+++ a.cpp
@@ -6,109 +6,41 @@ struct Stack
 {
     int size;
     int top;
-    char elements[SIZE];
+    string elements[SIZE];
 };
-void push( struct Stack *S,char val)
+void push( struct Stack *S,string val)
 {
-    if(S->top>= SIZE)
-    cout<<"Stack Full";
-    else
-    {
+   
         S->top=S->top+1;
         S->elements[S->top]=val;
-    }
 }
-char pop(struct Stack *S)
+string pop(struct Stack *S)
 {
-    char t;
-    if(S->top<0)
-    cout<<"Stack Empty";
-    else
+    string t;
+    
     t=S->elements[S->top--];
     return(t); 
-}
-char peek(struct Stack *S)
-{
-    char t;
-    if(S->top<0)
-    cout<<"Stack Empty";
-    else
-    t=S->elements[S->top];
-    return(t); 
-}
-int priorityCheck(char ch)
-{
-    if(ch=='+' || ch=='-')
-    return 1;
-    else if(ch=='*' || ch=='/' ||ch=='%')
-    return 2;
-    else return 0;
 }
 int main()
 {
     struct Stack *S1=new(struct Stack);
-    S1->size=SIZE-1;      
+    S1->size=SIZE-1;
     S1->top=-1;
-    
-    string s;
-    cin>>s;
-    char res[20];
-    int j=0;
-    for(int i=s.length()-1;i>=0;i--)
+
+    string sym;
+    cin>>sym;
+    for(int i=sym.length()-1;i>=0;i--)
     {
-        if(s[i]=='(')
-        res[j++]=')';
-        else if(s[i]==')')
-        res[j++]='(';
-        else 
-        res[j++]=s[i];
-    }
-    string sym(res);
-    for(int i=0;i<sym.length();i++)
-    {
+
         if(isdigit(sym.at(i)))
         {
-            cout<<(int)sym.at(i)-48;
+            push(S1,to_string((int)sym[i]-48));
+            
         }
         else
         {
-            if(sym.at(i)=='(')
-            {
-                push(S1,sym.at(i));
-            }
-            else if(sym.at(i)==')')
-            {
-                while(peek(S1)!='(')
-                {
-                    if(peek(S1)!='(')
-                    cout<<pop(S1);
-                    else pop(S1);
-                    if(S1->top==-1)
-                    break;
-                }
-                if(peek(S1)!='(')
-                cout<<pop(S1);
-                else pop(S1);
-            }
-            else if(S1->top==-1)
-            {
-                push(S1,sym.at(i));
-            }
-            else
-            {
-                if(priorityCheck(sym.at(i))>priorityCheck(peek(S1)))
-                {
-                    push(S1,sym.at(i));
-                }
-                else
-                {
-                    cout<<pop(S1);
-                    push(S1,sym.at(i));
-                }
-            }
-            
+            push(S1,'('+pop(S1)+sym.at(i)+pop(S1)+')');
         }
-        
     }
     while(S1->top>-1)
     cout<<pop(S1);
