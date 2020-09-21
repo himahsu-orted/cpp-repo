9863c9f5a8e68d12eb3766e2daa39a52e2d8a2dc
Himanshu Sah <hs@Himanshus-MacBook-Pro.local>
2020-09-21 20:44:09 +0545

    c++

--- b.cpp
+++ b.cpp
@@ -1,165 +1,82 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int SIZE=100;
-struct Queue
-{
-    int size;
-    int front;
-    int rear;
-    int elements[SIZE];
-};
-void enq( struct Queue *Q,int val)
-{
-    if((Q->rear+1)%Q->size==Q->front)
-    cout<<"Queue Full";
-    else
-    {
-        if(Q->front==-1)
-        {
-            Q->front=0;
-            Q->rear=0;
-        }
-        else
-        {
-            Q->rear=(Q->rear+1)%Q->size;
-        }
-        Q->elements[Q->rear]=val;
-    }
-}
-int deq(struct Queue *Q)
-{
-    int t;
-    if(Q->front==-1)
-    cout<<"Queue Empty";
-    else
-    {
 
-        if(Q->front==Q->rear)
-        {
-            t=Q->elements[Q->front];
-            Q->elements[Q->front]=0;
-            Q->front=-1;
-            Q->rear=-1;
-        }
+typedef struct node
+{
+    int data;
+    struct node *next;
+} * LL;
+void insert(LL START)
+{
+    LL END,CUR;
+    END=START;
+
+
+    int k;
+    char ch,n;
+    cin>>n;
+
+    if(n=='#')
+    return;
+
+    if(n=='0')
+    cin>>ch;
+    else 
+    cin>>k;
+
+    while(n!='#')
+    {
+        
+        CUR=new(struct node);
+        if(n=='0')
+        CUR->data=(int)ch;
         else 
-        {
-            t=Q->elements[Q->front];
-            Q->elements[Q->front]=0;
-            Q->front=(Q->front+1)%Q->size;
-        }
-    }
-
-    return t;
-}
-bool isSecondLast(struct Queue *Q)
-{
-    if(Q->front==Q->rear)
-    return true;
-    else 
-    return false;
-}
-void takeInput(struct Queue *Q1)
-{
-    string sym;
-    cin>>sym;
-    for(int i=0;i<sym.length();i++)
-    {
-        if(isdigit(sym[i]))
-        {
-            enq(Q1,(int)sym[i]-48);
-        }
-        else
-        {
-            switch(sym[i])
-            {
-                case '+':
-                    enq(Q1,5001);
-                    break;
-                case '-':
-                    enq(Q1,5002);
-                    break;
-                case '*':
-                    enq(Q1,5003);
-                    break;
-                case '/':
-                    enq(Q1,5004);
-                    break;
-            }
-        }
-    }
-}
-bool checkOperator(int f)
-{
-    if(f==5001||f==5002||f==5003||f==5004)
-    return true;
-    else 
-    return false;
-}
-bool checkOperand(int f)
-{
-    if(f==5001||f==5002||f==5003||f==5004)
-    return false;
-    else 
-    return true;
-}
-void storeVal(struct Queue *Q, int p1,int p2, int p3)
-{
-    switch (p1)
-    {
-        case 5001:
-            enq(Q,p2+p3);
-            break;
-        case 5002:
-            enq(Q,p2-p3);
-            break;
-        case 5003:
-            enq(Q,p2*p3);
-            break;
-        case 5004:
-            enq(Q,p2/p3);
-            break;
-        default:
-            break;
-    }
-}
-void calculate(struct Queue *Q)
-{
-    if(!isSecondLast(Q))
-    {
-        int p1,p2,p3,temp;
-        p1=deq(Q);
-        p2=deq(Q);
-        while(Q->front>-1)
-        {
-            p3=deq(Q);
-            if(checkOperator(p1) && checkOperand(p2) && checkOperand(p3))
-            { 
-                storeVal(Q,p1,p2,p3);
-                if(Q->front==Q->rear)
-                {
-                    cout<<deq(Q)<<endl;
-                    exit(0);
-                }
-                calculate(Q);   
-            }
-            else
-            {
-                enq(Q,p1);
-                p1=p2;
-                p2=p3;
-            }
-        }
-    }
+        CUR->data=k;
+        CUR->next=NULL;
+        END->next=CUR;
+        END=CUR;
+        cout<<"herer";
+        if(n=='#')
+        return;
     
+        if(n=='0')
+        cin>>ch;
+        else 
+        cin>>k;
+    }
+}
+void displayAll(LL S)
+{
+    if(S!=NULL)
+    {
+        
+        cout<<S->data<<" ";
+        displayAll(S->next);
+    }
+}
+
+struct node *createList()
+{
+    char ch;
+    LL L1;
+
+    cin>>ch;
+    L1=new(struct node);
+    L1->data=-1;
+    L1->next=NULL;
+    insert(L1);
+    return L1;
+}
+void removeFirst(LL (&S))
+{
+    S=S->next;
 }
 int main()
 {
-    struct Queue *Q1=new(struct Queue);
-    Q1->size=SIZE-1;
-    Q1->front=-1;
-    Q1->rear=-1;
-
-    takeInput(Q1);
-    calculate(Q1);
+    
+    LL C;
+    C=createList();
+    removeFirst(C);
+    displayAll(C);
 }
--- test.cpp
+++ test.cpp
@@ -1,73 +1,387 @@
 #include<bits/stdc++.h>
 using namespace std;
-void countSort(int arr[],int n,int place)
+
+
+typedef struct node
 {
-    int div;
-    div=pow(10,place);
-    int i;
-    int max=9;
-    int fx[max+1];
+    int data;
+    struct node *next;
+} * LL;
+void insert(LL START)
+{
+    LL END,CUR;
+    END=START;
+    int n;
+    cin>>n;
+    while(n>-1)
+    {
+        CUR=new(struct node);
+        CUR->data=n;
+        CUR->next=NULL;
+        END->next=CUR;
+        END=CUR;
+        cin>>n;
+    }
+}
+void displayAll(LL S)
+{
+    if(S!=NULL)
+    {
+        
+        cout<<S->data<<" ";
+        displayAll(S->next);
+    }
+}
+void deleteEnd(LL C)
+{
+    while(C!=NULL)
+    {
+        if((C->next->next)==NULL)
+        {
+            C->next=NULL;
+            break;
+        }
+        C=C->next;
+    }
+}
+void addInFront(LL (&START),int n)
+{
+    LL CUR;
+    CUR=new(struct node);
+    CUR->data=n;
+    CUR->next=START;
+    START=CUR;
+}
+int countNodes(LL C)
+{
+    int count=0;
+    while(C!=NULL)
+    {
+        count++;
+        C=C->next;
+    }
+    return count;
+}
+void palindrome(LL C)
+{
+    LL SP,CON;
+    SP=C;
+    CON=C;
+    while(C->next!=NULL)
+    {
+        C=C->next;
+        if(C->next!=NULL)
+        {
+            C=C->next;
+            SP=SP->next;
+        }
+        else 
+        break;
+    }
+    int count=0;
+    if(countNodes(SP)%2==0)
+    {
+        SP=SP->next;
+        count=1;
+    }
+    
+    while(SP!=NULL)
+    {
+        addInFront(CON,SP->data);
+        count++;
+        SP=SP->next;
+    }
+    while(count>1)
+    {  
+        deleteEnd(CON);
+        count--;
+    }
+    
+    LL CP,LP;
+    CP=CON;
+    LP=CON;
+    while(CP->next!=NULL)
+    {
+        CP=CP->next;
+        if(CP->next!=NULL)
+        {
+            CP=CP->next;
+            LP=LP->next;
+        }
+        else 
+        break;
+    }
+    LP=LP->next;
+    while(LP!=NULL)
+    {
+        if(LP->data!=CON->data)
+        {
+            cout<<0;
+            return;
+        }
+        else
+        {
+            LP=LP->next;
+            CON=CON->next;
+        }
+    }
+    cout<<1;
+}
+void deleteK(LL C,int k)
+{
+    LL SP;
+    SP=C;
+    
+        while(C!=NULL)
+        {
+            if(C->data==k)
+            {
+                while(SP!=NULL)
+                {
+                    if(SP->next==C)
+                    {
+                        SP->next=C->next;
+                        return;
+                    }
+                    SP=SP->next;
+                }
+            }
+            C=C->next;
+        }
+    
+}void removeLastDup(LL C)
+{
+    LL PER=C;
+    LL FOUND=C;
+    while(PER->next!=NULL)
+    {
+        while(C->next!=NULL)
+        {
+            if(C->next->data==PER->data)
+            {
+                FOUND=C;
+            }
+            C=C->next;
+        }
+        if(FOUND!=PER)
+        {
+            if(FOUND->next->next!=NULL)
+            FOUND->next=FOUND->next->next;
+            else 
+            {
+                FOUND->next=NULL;
+            }
+        }
+        if(PER->next!=NULL)
+        {
+            PER=PER->next;
+            C=PER;
+            FOUND=C;
+        }
+        else break;
+        
+    }
+}
+void removeUnsortedDup(LL C)
+{
+    LL PER=C;
+    while(PER->next!=NULL)
+    {
+        while(C->next!=NULL)
+        {
+            if(C->next->data==PER->data)
+            {
+                if(C->next->next!=NULL)
+                C->next=C->next->next;
+                else 
+                {
+                    C->next=NULL;
+                    break;
+                }
+            }
+            C=C->next;
+        }
+        if(PER->next!=NULL)
+        PER=PER->next;
+        else break;
+        C=PER;
+    }
+}
+void removeSortedDup(LL C,LL PER)
+{
 
-    for(i=0;i<max+1;i++)
-    fx[i]=0;
+    if(C->next!=NULL)
+    {
+        if(PER->data==C->next->data)
+        {
+            C=C->next;
+        }
+        else
+        {
+            PER->next=C->next;
+            C=C->next;
+            PER=C;
+        }
+        removeSortedDup(C,PER);
+    }
+    else 
+    PER->next=NULL;
+}
 
-    for(i=0;i<n;i++)
+struct node *createList()
+{
+    int n;
+    LL L1;
+
+    cin>>n;
+    L1=new(struct node);
+    L1->data=n;
+    L1->next=NULL;
+    insert(L1);
+
+    return L1;
+}
+struct node *evenOdd(LL C)
+{
+    LL R=new(struct node),TEMP=C;
+    LL END,CUR;
+    END=R;
+    CUR=R;
+    while(C!=NULL)
     {
-        fx[(arr[i]/div)%10]++;
+        if(C->data%2==0)
+        {
+            CUR->data=C->data;
+            END=new(struct node);
+            CUR->next=END;
+            CUR=END;
+        }
+        C=C->next;
     }
-    cout<<endl;
-    for(i=1;i<max+1;i++)
+    while(TEMP!=NULL)
     {
-        fx[i]+=fx[i-1];
+        if(TEMP->data%2!=0)
+        {
+            CUR->data=TEMP->data;
+            if(TEMP->next!=NULL)
+            END=new(struct node);
+            CUR->next=END;
+            CUR=END;
+        }
+        TEMP=TEMP->next;
     }
-    int B[n],pos;
-    for(i=n-1;i>=0;i--)
+    deleteEnd(R);
+    return R;
+    
+}
+void insertBefore(LL C,int n,int k)
+{
+    LL SP,ADD;
+    SP=C;
+    ADD=new(struct node);
+    while(C!=NULL)
     {
-        pos=fx[(arr[i]/div)%10]-1;
-        fx[(arr[i]/div)%10]--;
-        B[pos]=arr[i];
+        if(n==C->data)
+        {
+            while(SP!=NULL)
+            {
+                if(SP->next==C)
+                {
+                    ADD->data=k;
+                    ADD->next=C;
+                    SP->next=ADD;
+                    return;
+                }
+                SP=SP->next;
+            }
+        }
+        C=C->next;
     }
-    for(i=0;i<n;i++)
+}
+struct node *insSort(LL C)
+{
+    LL R=new(struct node);
+    LL END,CUR,TEMP;
+    END=R;
+    CUR=R;
+
+    R->data=C->data;
+    R->next=NULL;
+
+    C=C->next;
+    while(C!=NULL)
     {
-        arr[i]=B[i];
+        if(C->data>=END->data)
+        {
+            CUR=new(struct node);
+            CUR->data=C->data;
+            END->next=CUR;
+            END=CUR;
+        }
+        else 
+        {
+            TEMP=R;
+            if(C->data<R->data)
+            {
+                addInFront(R,C->data);
+            }   
+            else
+            {
+                while(TEMP!=NULL)
+                {
+                    if(TEMP->data>C->data)
+                    {
+                        insertBefore(R,TEMP->data,C->data);
+                        break;
+                    }
+                    TEMP=TEMP->next;
+                }
+            }
+        }
+        C=C->next;
     }
+    return R;
+
+
+}
+
+void rev(LL C,LL PRE)
+{
+    if(C->next!=NULL)
+    {
+        rev(C->next,C);
+    }
+    C->next=PRE;  
 }
 int main()
 {
-    int n;
-    cout<<"Enter the size of array: ";
-    cin>>n;
-    int arr[n],A[n];
-    int i,max;
-    for(i=0;i<n;i++)
-    {
-        cin>>A[i];
-        if(i==0)
-        max=A[i];
-        else
-        {
-            if(A[i]>max)
-            max=A[i];
-        }
-    }
-    int num=max;
-    int count=0;
-    while(num>0)
-    {
-        count++;
-        num=num/10;
-        
-    };
-    for(i=0;i<n;i++)
-    {
-        arr[i]=A[i];
-    }
-    for(i=0;i<count;i++)
-    {
-        countSort(arr,n,i);
-    }
-    for(int j=0;j<n;j++)
-    {
-        cout<<arr[j]<<" ";
-    }
+    
+    LL C;
+    // C=createList();
+    // palindrome(C);
+
+    // C=createList();
+    // removeSortedDup(C,C);
+    // displayAll(C);
+
+    // C=createList();
+    // removeUnsortedDup(C);
+    // displayAll(C);
+
+    // C=createList();
+    // removeLastDup(C);
+    // displayAll(C);
+
+    // C=createList();
+    // C=evenOdd(C);
+    // displayAll(C);
+
+    // C=createList();
+    // C=insSort(C);
+    // displayAll(C);
+    
+    C=createList();
+    rev(C,NULL);
+    C->next=NULL;
+    displayAll(C);
 }
